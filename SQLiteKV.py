6ec5ae2b7c92ddc03245de6a137857b017f50645
raymo <raymo@YOGA3>
2019-09-04 21:31:02 -0700

    initial

---
+++ SQLiteKV.py
@@ -0,0 +1,9 @@
+#######
+#Barebones SQLite
+#######
+import json
+from sqlitedict import SqliteDict
+def db(filename = "unnamed.sqlite"):
+    return SqliteDict(filename)
+def jsonDB(filename = "unnamed.sqlite"):
+    return SqliteDict(filename,encode = json.dumps,decode = json.loads)
