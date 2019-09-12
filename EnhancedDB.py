dd8d10c19d5e87a55b6ac9d4dc1056ed73a95973
raymo <raymo@YOGA3>
2019-09-11 22:10:14 -0700

    lemora update with enhancer

---
+++ EnhancedDB.py
@@ -0,0 +1,11 @@
+#######
+#Barebones SQLite
+#######
+import json,bz2,sqlite3
+from sqlitedict import SqliteDict
+def encode(data):
+    return sqlite3.Binary(bz2.compress(json.dumps(data).encode()))
+def decode(data):
+    return json.loads(bz2.decompress(data).decode())
+def db(filename = "unnamed.sqlite"):
+    return SqliteDict(filename,encode = encode,decode = decode)
--- lemora.py
+++ lemora.py
@@ -1,1 +1,2 @@
 import SQLiteKV
+import EnhancedDB
