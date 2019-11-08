25414cae68eb744fe1dba589000acb6b45b8f1a0
Raymond <20248577+javaarchive@users.noreply.github.com>
2019-11-08 11:07:55 -0800

    Update SQLiteKV.py

--- SQLiteKV.py
+++ SQLiteKV.py
@@ -6,4 +6,4 @@ from sqlitedict import SqliteDict
 def db(filename = "unnamed.sqlite"):
     return SqliteDict(filename)
 def jsonDB(filename = "unnamed.sqlite"):
-    return SqliteDict(filename,encode = json.dumps,decode = json.loads)
+    return SqliteDict(filename, encode = json.dumps, decode = json.loads)
